63be1bbe8166063cbd7d15cb5d23da8650fc7ac2
Kofi Frimpong <noeljoel61@gmail.com>
2023-05-19 02:41:58 +0000

    Mod: Modify views.py

--- netcodeapi/views.py
+++ netcodeapi/views.py
@@ -3,12 +3,15 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.permissions import IsAuthenticated
 from .serializers import CodeSerializer, UserSerializer
 from .models import Code, User
 from django.db.models import Q
 # from django.contrib.auth.models import User
 
 # Create your views here.
+
+
 class EndPoints(APIView):
     def get(self, request):
         endpoints = [
@@ -18,6 +21,7 @@ class EndPoints(APIView):
             "codes/<id>/",
         ]
         return Response(endpoints)
+
 
 class RegisterView(APIView):
     def get(self, request):
@@ -32,6 +36,7 @@ class RegisterView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
 
+
 class LoginView(APIView):
     def post(self, request):
         username = request.data.get('username')
@@ -41,13 +46,16 @@ class LoginView(APIView):
 
         if user is None:
             raise AuthenticationFailed("User not found")
-        
+
         if not user.check_password(password):
             raise AuthenticationFailed("Password is incorrect")
-        
+
         return Response({"message": "user verified"})
 
+
 class GetCodes(APIView):
+    permission_classes = [IsAuthenticated,]
+
     def get(self, request):
         # user = request.user
         codes = Code.objects.all()
@@ -55,7 +63,7 @@ class GetCodes(APIView):
 
         if search_param:
             codes = codes.filter(
-                Q(topic__icontains=search_param) | 
+                Q(topic__icontains=search_param) |
                 Q(code__icontains=search_param) |
                 Q(url__icontains=search_param) |
                 Q(author__icontains=search_param),
@@ -63,7 +71,7 @@ class GetCodes(APIView):
 
         serializer = CodeSerializer(codes, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
+
     def post(self, request):
         data = CodeSerializer(data=request.data)
         if data.is_valid():
@@ -71,13 +79,14 @@ class GetCodes(APIView):
             return Response(data.data, status=status.HTTP_201_CREATED)
         return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class CodeDetail(APIView):
     def get_object(self, id):
         try:
             return Code.objects.get(id=id)
         except Code.DoesNotExist:
             raise Response("Object not found")
-    
+
     def get(self, request, id):
         code = self.get_object(id)
         serializer = CodeSerializer(code)
@@ -90,7 +99,7 @@ class CodeDetail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
     def delete(self, request, id):
         code = self.get_object(id)
         code.delete()
